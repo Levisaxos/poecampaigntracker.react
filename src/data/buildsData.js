3adeea3522bbdbb394740af4b847f07522613f59
PSTheun <123176675+PSTheun@users.noreply.github.com>
2025-08-29 10:33:18 +0200

    Added lightning arrow build

--- src/data/buildsData.js
+++ src/data/buildsData.js
@@ -1,130 +1,293 @@
-export const buildsData = {
-  builds: [
+export const buildsData = {  
+  "builds": [
     {
-      buildId: 1,
-      buildName: "Essence Drain - Contagion",
-      description: "A damage over time focused build using Essence Drain and Contagion combo",
-      skills: [
+      "buildId": 1,
+      "buildName": "Essence Drain - Contagion",
+      "description": "A damage over time focused build using Essence Drain and Contagion combo",
+      "skills": [
         {
-          skillId: 1,
-          skillName: "Essence Drain",
-          levelRequirement: 6,
-          uncutSkillGemLevel: 3,
-          supports: [            
+          "skillId": 1,
+          "skillName": "Essence Drain",
+          "levelRequirement": 6,
+          "uncutSkillGemLevel": 3,
+          "supports": [            
             {
-              supportId: 1,
-              supportName: "Swift Affliction",
-              uncutSupportGemLevel: 1
+              "supportId": 1,
+              "supportName": "Swift Affliction",
+              "uncutSupportGemLevel": 1
             },
             {
-              supportId: 2,
-              supportName: "Zenith",
-              uncutSupportGemLevel: 1
+              "supportId": 2,
+              "supportName": "Zenith",
+              "uncutSupportGemLevel": 1
             },
             {
-              supportId: 2,
-              supportName: "Chain",
-              uncutSupportGemLevel: 1
+              "supportId": 3,
+              "supportName": "Chain",
+              "uncutSupportGemLevel": 1
             }
           ]
         },
         {
-          skillId: 2,
-          skillName: "Contagion",
-          levelRequirement: 1,
-          uncutSkillGemLevel: 1,
-          supports: [
+          "skillId": 2,
+          "skillName": "Contagion",
+          "levelRequirement": 1,
+          "uncutSkillGemLevel": 1,
+          "supports": [
             {
-              supportId: 1,
-              supportName: "Unleash",
-              uncutSupportGemLevel: 1
+              "supportId": 1,
+              "supportName": "Unleash",
+              "uncutSupportGemLevel": 1
             },
             {
-              supportId: 2,
-              supportName: "Magnified Effect",
-              uncutSupportGemLevel: 1
+              "supportId": 2,
+              "supportName": "Magnified Effect",
+              "uncutSupportGemLevel": 1
             },
             {
-              supportId: 3,
-              supportName: "Hinder",
-              uncutSupportGemLevel: 1
+              "supportId": 3,
+              "supportName": "Hinder",
+              "uncutSupportGemLevel": 1
             }
           ]
         },
         {
-          skillId: 3,
-          skillName: "Fire Wall",
-          levelRequirement: 6,
-          uncutSkillGemLevel: 3,
-          supports: []
+          "skillId": 3,
+          "skillName": "Fire Wall",
+          "levelRequirement": 6,
+          "uncutSkillGemLevel": 3,
+          "supports": []
         },
         {
-          skillId: 4,
-          skillName: "Withering Presence",
-          levelRequirement: 11,
-          uncutSkillGemLevel: 5,
-          supports: [
+          "skillId": 4,
+          "skillName": "Withering Presence",
+          "levelRequirement": 11,
+          "uncutSkillGemLevel": 5,
+          "supports": [
             {
-              supportId: 1,
-              supportName: "Persistaece",
-              uncutSupportGemLevel: 1
+              "supportId": 1,
+              "supportName": "Persistaece",
+              "uncutSupportGemLevel": 1
             }
           ]
         },
         {
-          skillId: 5,
-          skillName: "Bonestorm",
-          levelRequirement: 16,
-          uncutSkillGemLevel: 5,
-          supports: [
+          "skillId": 5,
+          "skillName": "Bonestorm",
+          "levelRequirement": 16,
+          "uncutSkillGemLevel": 5,
+          "supports": [
             {
-              supportId: 1,
-              supportName: "Scattershot",
-              uncutSupportGemLevel: 1
+              "supportId": 1,
+              "supportName": "Scattershot",
+              "uncutSupportGemLevel": 1
             },
             {
-              supportId: 2,
-              supportName: "Brutality",
-              uncutSupportGemLevel: 1
+              "supportId": 2,
+              "supportName": "Brutality",
+              "uncutSupportGemLevel": 1
             }
           ]
         },
         {
-          skillId: 6,
-          skillName: "Dark Effigy",
-          levelRequirement: 6,
-          uncutSkillGemLevel: 3,
-          supports: [
+          "skillId": 6,
+          "skillName": "Dark Effigy",
+          "levelRequirement": 6,
+          "uncutSkillGemLevel": 3,
+          "supports": [
             {
-              supportId: 1,
-              supportName: "Overabundance",
-              uncutSupportGemLevel: 1
+              "supportId": 1,
+              "supportName": "Overabundance",
+              "uncutSupportGemLevel": 1
             },
             {
-              supportId: 2,
-              supportName: "Arcane Tempo",
-              uncutSupportGemLevel: 1
+              "supportId": 2,
+              "supportName": "Arcane Tempo",
+              "uncutSupportGemLevel": 1
             }
           ]
         },
         {
-          skillId: 1,
-          skillName: "Despair",
-          levelRequirement: 16,
-          uncutSkillGemLevel: 9,
-          supports: [
+          "skillId": 7,
+          "skillName": "Despair",
+          "levelRequirement": 16,
+          "uncutSkillGemLevel": 9,
+          "supports": [
             {
-              supportId: 1,
-              supportName: "Heightened Curse",
-              uncutSupportGemLevel: 1
+              "supportId": 1,
+              "supportName": "Heightened Curse",
+              "uncutSupportGemLevel": 1
             },
             {
-              supportId: 2,
-              supportName: "Cursed Ground",
-              uncutSupportGemLevel: 1
+              "supportId": 2,
+              "supportName": "Cursed Ground",
+              "uncutSupportGemLevel": 1
             }
           ]
         }        
+      ]
+    },
+    {
+      "buildId": 2,
+      "buildName": "Lightning Arrow Deadeye",
+      "description": "A ranged bow build focused on Lightning Arrow and Lightning Rod combo with high clear speed",
+      "skills": [
+        {
+          "skillId": 1,
+          "skillName": "Lightning Arrow",
+          "levelRequirement": 2,
+          "uncutSkillGemLevel": 1,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Chain",
+              "uncutSupportGemLevel": 1
+            },
+            {
+              "supportId": 2,
+              "supportName": "Martial Tempo",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 2,
+          "skillName": "Lightning Rod",
+          "levelRequirement": 2,
+          "uncutSkillGemLevel": 1,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Scattershot",
+              "uncutSupportGemLevel": 1
+            },
+            {
+              "supportId": 2,
+              "supportName": "Concentrated Effect",
+              "uncutSupportGemLevel": 1
+            },
+            {
+              "supportId": 3,
+              "supportName": "Primal Armament",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 3,
+          "skillName": "Bow Shot",
+          "levelRequirement": 1,
+          "uncutSkillGemLevel": 1,
+          "supports": []
+        },
+        {
+          "skillId": 4,
+          "skillName": "Contagion",
+          "levelRequirement": 6,
+          "uncutSkillGemLevel": 3,
+          "supports": []
+        },
+        {
+          "skillId": 5,
+          "skillName": "Herald of Thunder",
+          "levelRequirement": 10,
+          "uncutSkillGemLevel": 5,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Innervate",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 6,
+          "skillName": "Barrage",
+          "levelRequirement": 14,
+          "uncutSkillGemLevel": 5,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Premeditation",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 7,
+          "skillName": "Freezing Salvo",
+          "levelRequirement": 14,
+          "uncutSkillGemLevel": 5,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Hourglass",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 8,
+          "skillName": "Voltaic Mark",
+          "levelRequirement": 22,
+          "uncutSkillGemLevel": 5,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Persistence",
+              "uncutSupportGemLevel": 1
+            },
+            {
+              "supportId": 2,
+              "supportName": "Magnified Effect",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 9,
+          "skillName": "Wind Dancer",
+          "levelRequirement": 31,
+          "uncutSkillGemLevel": 5,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Blind",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 10,
+          "skillName": "Tame Beast",
+          "levelRequirement": 31,
+          "uncutSkillGemLevel": 5,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Meat Shield",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        },
+        {
+          "skillId": 11,
+          "skillName": "Tornado Shot",
+          "levelRequirement": 41,
+          "uncutSkillGemLevel": 5,
+          "supports": [
+            {
+              "supportId": 1,
+              "supportName": "Overabundance",
+              "uncutSupportGemLevel": 1
+            },
+            {
+              "supportId": 2,
+              "supportName": "Maim",
+              "uncutSupportGemLevel": 1
+            }
+          ]
+        }
       ]
     }
   ]
